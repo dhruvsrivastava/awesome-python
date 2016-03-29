6f79adfd7fb68f33c52306e332a1128f4e2abe11
Dhruv Srivastava <dhruv.10000@gmail.com>
2016-03-29 08:24:35 +0530

    multiple requests if error message is recieved

--- codechef-scraper/scrape.py
+++ codechef-scraper/scrape.py
@@ -1,10 +1,11 @@
 from bs4 import BeautifulSoup
-from urllib2 import urlopen
+from urllib2 import urlopen , HTTPError
 
 id_dict = {}
 
 #username
 username = "ironmandhruv"
+all_submissions = 0
 
 for year in range(2013 , 2017) :
 	page = 0;
@@ -12,21 +13,34 @@ for year in range(2013 , 2017) :
 
 		print "current page %d" %(page)
 		url = "https://www.codechef.com/submissions?page="+ str(page)+ "sort_by=All&sorting_order=asc&language=All&status=All&year="+str(year)+"&handle="+username+"&pcode=&ccode=&Submit=GO"
-		try:
-			url = urlopen(url)
-		except urllib2.HTTPError:
-			print "error "
+		connected = False
+		while not connected:
+			try:
+				url = urlopen(url)
+				connected = True
+			except HTTPError:
+				print "error "
 		soup = BeautifulSoup(url)
 
 		for link in soup.find_all('tr' , class_ = "kol"):
+
 			child = link.find_all('td')
+
+			if len(child) == 0:
+				continue
+
 			for j in range(len(child)):
 				print child[j].string
+			print child[0].string
 			if child[0].string in id_dict:
 				page = 1000
 				break
 			else:
+				all_submissions += 1
 				id_dict[child[0].string] = 1
+
+			if len(child) == 1:
+				continue
 			
 			timestamp = str(child[1].string)
 			# print type(timestamp)
@@ -49,6 +63,9 @@ for year in range(2013 , 2017) :
 			hours = int(hours)
 			minutes = int(minutes)
 		page += 1
+
+
+print "All submissions %d" %(all_submissions)
 
 	# print "%d %d" %(hours , minutes)
 
